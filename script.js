b64d9e21e5beea8b05be9149580399179764c00b
LauraDuf <34089265+LauraDuf@users.noreply.github.com>
2018-03-01 09:42:05 +0100

    V2

--- script.js
+++ script.js
@@ -1,30 +1,28 @@
-$(document).keydown(function bougehaut(e) {
+$(document).keydown(function bouge(e) {
   if (e.keyCode == 38) {
   $('#flappy').animate({
     'margin-bottom': '+=100px'},
     speed=100, 'swing');
-}});
-
-$(document).keydown(function bougebas(e) {
-  if (e.keyCode == 40) {
-  $('#flappy').animate({
-    'margin-top': '+=100px'},
-    speed=100, 'swing');
-}});
-
-$(document).keydown(function bougedroite(e) {
+}
+  if (e.keyCode == 40){
+    $('#flappy').animate({
+      'margin-top': '+=100px'},
+      speed=100, 'swing');
+  }
   if (e.keyCode == 39) {
-  $('#flappy').animate({
-    'margin-left': '+=100px'},
-    speed=100, 'swing');
-}});
+    $('#flappy').animate({
+      'margin-left': '+=100px'},
+      speed=100, 'swing');
+  }
 
-$(document).keydown(function bougegauche(e) {
   if (e.keyCode == 37) {
-  $('#flappy').animate({
-    'margin-right': '+=100px'},
-    speed=100, 'swing');
-}});
+    $('#flappy').animate({
+      'margin-right': '+=100px'},
+      speed=100, 'swing');
+    }
+});
+
+
 
 $("#flappy").click(function coucou(){
     alert('Coucou')
